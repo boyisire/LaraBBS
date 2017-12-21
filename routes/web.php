9a1410cc9f24b1dd8f47f1204608d44efb041a37
大业 <boyisire@foxmail.com>
2017-12-21 19:01:30 +0800

    添加话题类型列表

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,9 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-//User
+//用户
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-
+//话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+//话题分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
